3c69547e8e40b7e5989ad2a6ab3acab125787223
Arun <arunchauhan54@gmail.com>
2015-12-06 17:23:58 -0600

    Added JMS message Adapter

--- spring-integration-sample/src/main/java/com/arun/singh/sample/spring/integration/config/MainConfiguration.java
+++ spring-integration-sample/src/main/java/com/arun/singh/sample/spring/integration/config/MainConfiguration.java
@@ -1,14 +1,72 @@
 package com.arun.singh.sample.spring.integration.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.jms.Jms;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
 
 /**
  * Created by Arun on 12/5/2015.
  */
 @Configuration
+@EnableIntegration
 @PropertySource("basic.properties")
 public class MainConfiguration {
 
 
+    public static void main(String[] args) throws Exception {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MainConfiguration.class);
+
+    }
+
+
+    @Bean
+    public ActiveMQConnectionFactory activeMQConnectionFactory() {
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
+        activeMQConnectionFactory.setBrokerURL("tcp://localhost:61616");
+        return activeMQConnectionFactory;
+    }
+
+    @Bean
+    public CachingConnectionFactory cachingConnectionFactory(){
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(this.activeMQConnectionFactory());
+        cachingConnectionFactory.setSessionCacheSize(10);
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(){
+        JmsTemplate jmsTemplate = new JmsTemplate();
+        jmsTemplate.setConnectionFactory(this.cachingConnectionFactory());
+        jmsTemplate.setDeliveryPersistent(true);
+//        jmsTemplate.setMessageConverter();
+        return jmsTemplate;
+    }
+
+
+
+//
+//    @Bean
+//    public IntegrationFlow process1() {
+//        return IntegrationFlows.from(Jms.inboundAdapter(this.jmsTemplate()).destination("arun.test.queue1"))
+//                .handle(h -> System.out.println(h.getPayload()))
+//                .get();
+//    }
+
+
+    @Bean
+    public IntegrationFlow process() {
+        return IntegrationFlows.from(Jms.messageDriverChannelAdapter(cachingConnectionFactory()).destination("arun.test.queue1"))
+                .handle(h -> System.out.println(h.getPayload()))
+                .get();
+    }
 
 }
