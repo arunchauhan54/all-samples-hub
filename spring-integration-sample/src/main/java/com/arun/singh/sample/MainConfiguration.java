eef21a36adc0cd7c134dc4e4e9ce211afba29a97
Arun <arunchauhan54@gmail.com>
2015-12-05 20:19:20 -0600

    Configuration file added

---
+++ spring-integration-sample/src/main/java/com/arun/singh/sample/MainConfiguration.java
@@ -0,0 +1,14 @@
+package com.arun.singh.sample;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created by Arun on 12/5/2015.
+ */
+@Configuration
+@PropertySource("basic.properties")
+public class MainConfiguration {
+
+
+
+}
