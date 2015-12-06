50e5300636d3fada903cf0478f165cd88d75aa66
Arun <arunchauhan54@gmail.com>
2015-12-05 20:21:59 -0600

    initial project structure checkin

--- spring-integration-sample/src/main/java/com/arun/singh/sample/spring/integration/config/MainConfiguration.java
+++ spring-integration-sample/src/main/java/com/arun/singh/sample/spring/integration/config/MainConfiguration.java
@@ -1,4 +1,4 @@
-package com.arun.singh.sample;
+package com.arun.singh.sample.spring.integration.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
