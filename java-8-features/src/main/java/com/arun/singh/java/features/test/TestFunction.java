3bfa494a10cdd0682319838d5fece46be4cecb35
Arun <arunchauhan54@gmail.com>
2016-01-01 17:12:34 -0600

    Adding test Function

---
+++ java-8-features/src/main/java/com/arun/singh/java/features/test/TestFunction.java
@@ -0,0 +1,11 @@
+package com.arun.singh.java.features.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestFunction {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+
+    }
+}
