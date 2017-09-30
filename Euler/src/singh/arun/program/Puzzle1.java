f091c3e031b92a5097cdc0e5ff26c9b18e30008f
Arun <arunchauhan54@gmail.com>
2017-09-30 18:58:46 -0500

    updated stream uses

--- Euler/src/singh/arun/program/Puzzle1.java
+++ Euler/src/singh/arun/program/Puzzle1.java
@@ -1,22 +1,17 @@
 package singh.arun.program;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.function.LongPredicate;
 import java.util.stream.LongStream;
 
 public class Puzzle1 {
 
     public static void main(String[] args) {
-        final Collection<LongPredicate> listOfPredicate = new ArrayList<>();
-        listOfPredicate.add(isDivisibleFrom(3));
-        listOfPredicate.add((isDivisibleFrom(5)));
-        System.out.print(Puzzle1.sumOfValues(1000, listOfPredicate));
+        System.out.print(Puzzle1.sumOfValues(1000, isDivisibleFrom(3).and(isDivisibleFrom(5))));
     }
 
-    public static long sumOfValues(final long limit, Collection<LongPredicate> listOfPredicate) {
+    public static long sumOfValues(final long limit, LongPredicate p) {
         return LongStream.range(1, limit)
-                .filter(listOfPredicate.stream().reduce(LongPredicate::or).orElse(o -> false))
+                .filter(p)
                 .sum();
     }
 
